2c1a5744aaa6b428903b38e6d1f59b1cffe191f6
kimjinsoo <1004kjs_@naver.com>
2021-02-11 17:13:55 +0900

    routing 완료

---
+++ express_3/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const admin   = require('./routes/admin')
+const contact   = require('./routes/contact')
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('hello express')
+});
+
+app.use('/admin', admin);
+app.use('/contact', contact);
+
+
+
+app.listen( port, () => {
+    console.log('Express listhening on port', port)
+})
